6d541982e4fd31b526c10656d13b44d445a193bd
dcobbley <dcobbley@ruby.cat.pdx.edu>
2015-04-29 13:56:00 -0700

    finish part1

--- hw2/program1.s
+++ hw2/program1.s
@@ -4,22 +4,38 @@
 f:
 
 ### This is where your code begins ...
+		movl	$0,%eax			#init eax to zero incase total sum = 0
+		movl	(%rdi), %ecx	#number of elements in array
+		cmpl	$0, %ecx		#check if array contains zero elements
+		je		next
 
-		movl	(%rdi), %ebx	#number of elements in array
+loop:	addq	$4,%rdi			#add 4 to rdi
+		addl	(%rdi), %eax	#put  element from array
+		decl	%ecx			#decrement ebx 
+		cmpl	$0, %ecx		#check if ebx is zero - done
 
-loop:		addq	$4,%rdi		#add 4 to rdi
-		addl	(%rdi), %eax	#put  element into array
-		decl	%ebx			#decrement ebx 
-		cmpl	$0, %ebx			#check if ebx is zero - done
-		je		done
+		je		next
+
 		jmp 	loop			#else loop
+
+next:
+#Now do the same for rsi
+		movl	(%rsi), %ecx	#number of elements in array
+		cmpl	$0, %ecx		#check if array contains zero elements
+		je		done
+
+secloop:	addq	$4,%rsi			#add 4 to rdi
+		addl	(%rsi), %eax	#put  element from array
+		decl	%ecx			#decrement ebx 
+		cmpl	$0, %ecx		#check if ebx is zero - done
+
+		je		done
+
+		jmp 	secloop			#else loop
 
 
 
 done:
-
-
-				#Do the same for rsi
 ### This is where your code ends ...
 
 	ret
